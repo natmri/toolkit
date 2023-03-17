30cd2ec8a7c902d5b6fca44f387a7e27063d5d3f
starknt <1431880400@qq.com>
2023-03-17 15:03:59 +0800

    fix: get window native handle endian error

--- packages/platform/src/index.ts
+++ packages/platform/src/index.ts
@@ -1,12 +1,12 @@
 import type { BrowserWindow } from 'electron'
 import type { InputEvent } from '@natmri/platform-napi'
 import { setupInteractiveWindow } from '@natmri/platform-napi'
-export { setMainWindowHandle, releaseShutdownBlock, acquireShutdownBlock, insertWndProcHook } from '@natmri/platform-napi'
+export { setMainWindowHandle, releaseShutdownBlock, acquireShutdownBlock, insertWndProcHook, removeWndProcHook } from '@natmri/platform-napi'
 export { restoreInteractiveWindow as destroyInteractiveWindow } from '@natmri/platform-napi'
 export type { InputEvent, RawEvent } from '@natmri/platform-napi'
 
 export function createInteractiveWindow(window: BrowserWindow, callback?: (err: Error | null, event: InputEvent) => void): void {
-  const windowHandle = window.getNativeWindowHandle().readBigUInt64BE()
+  const windowHandle = window.getNativeWindowHandle().readBigUint64LE()
 
   setupInteractiveWindow(windowHandle, callback)
 }
